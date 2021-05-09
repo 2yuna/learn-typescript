51da34e7dc00231fdf4c9f4a9560260ef2289f46
Yuna Lee <y.duck10002@gmail.com>
2021-05-09 13:17:35 +0900

    2.10 진정한 타입스크립트의 시작! Union 타입

--- 1-types/1-4-alias.ts
+++ 1-types/1-4-alias.ts
@@ -21,6 +21,8 @@
   /**
    * String Literal Types
    * 타입을 문자열로도 지정할 수 있다!
+   *
+   * ✏️ 해당 타입으로 선언한 변수는 타입이 담고 있는 문자열의 값만을 담을 수 있다.
    */
   type Name = "name";
   let ellieName: Name;
@@ -28,4 +30,7 @@
 
   type JSON = "json";
   const json: JSON = "json";
+
+  type Boal = true;
+  const isCat: Boal = true;
 }
---
+++ 1-types/1-5-union.ts
@@ -0,0 +1,38 @@
+// 2.10 진정한 타입스크립트의 시작! Union 타입
+{
+  /**
+   * Union Types: OR
+   */
+  type Direction = "left" | "right" | "up" | "down";
+  function move(direction: Direction) {
+    console.log(direction);
+  }
+  move("down");
+
+  type TileSize = 8 | 16 | 32;
+  const tile: TileSize = 16;
+
+  // function: login -> success, fail
+  type SuccessState = {
+    response: {
+      body: string;
+    };
+  };
+  type FailState = {
+    reason: string;
+  };
+  type LoginState = SuccessState | FailState;
+
+  //   function login(id: string, password: string): Promise<LoginState> {
+  function login(id: string, password: string): LoginState {
+    return {
+      response: {
+        body: "logged in!",
+      },
+    };
+  }
+
+  // printLoginState(state: LoginState)
+  // success -> 🎉 body
+  // fail -> 😭 reason
+}
