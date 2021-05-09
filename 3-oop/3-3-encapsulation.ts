fc9b20e30d567f8be430e42a7c76fc265f2ee532
Yuna Lee <y.duck10002@gmail.com>
2021-05-09 17:42:14 +0900

    4.9 Encapsulation 캡슐화 시켜보기 💡

---
+++ 3-oop/3-3-encapsulation.ts
@@ -0,0 +1,44 @@
+//4.8 Encapsulation 캡슐화 시켜보기 💡
+{
+  type CoffeeCup = {
+    shots: number;
+    hasMilk: boolean;
+  };
+
+  // public
+  // private
+  // protected
+  class CoffeeMaker {
+    private static BEANS_GRAM_PER_SHOT: number = 7; //class level
+    private coffeeBeans: number = 0; // instance (object) level
+
+    private constructor(coffeeBeans: number) {
+      this.coffeeBeans = coffeeBeans;
+    }
+
+    static makeMachine(coffeBeans: number): CoffeeMaker {
+      return new CoffeeMaker(coffeBeans);
+    }
+
+    fillCoffeeBeans(beans: number) {
+      if (beans < 0) {
+        throw new Error("value for beans shuld be greater than 0");
+      }
+      this.coffeeBeans += beans;
+    }
+
+    makeCoffee(shots: number): CoffeeCup {
+      if (this.coffeeBeans < shots * CoffeeMaker.BEANS_GRAM_PER_SHOT) {
+        throw new Error("Not enough coffee beans!");
+      }
+
+      this.coffeeBeans -= shots * CoffeeMaker.BEANS_GRAM_PER_SHOT;
+      return {
+        shots,
+        hasMilk: false,
+      };
+    }
+  }
+
+  const maker = CoffeeMaker.makeMachine(32);
+}
