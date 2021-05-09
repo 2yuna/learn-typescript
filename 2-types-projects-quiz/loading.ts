4e241588b1bc3d764dc58c422cd8404fdcc44aeb
Yuna Lee <y.duck10002@gmail.com>
2021-05-09 16:53:35 +0900

    3.7 로딩 상태 표시 💡

--- 2-types-projects-quiz/loading.ts
+++ 2-types-projects-quiz/loading.ts
@@ -24,16 +24,16 @@
   printLoginState({ state: "success", response: { body: "loaded" } }); // 😃 loaded
   printLoginState({ state: "fail", reason: "no network" }); // 😱 no network
 
-  function printLoginState(type: ResourceLoadState) {
-    switch (type.state) {
+  function printLoginState(state: ResourceLoadState) {
+    switch (state.state) {
       case "loading":
         console.log(`👀 loading...`);
         break;
       case "success":
-        console.log(`😃 ${type.response.body}`);
+        console.log(`😃 ${state.response.body}`);
         break;
       case "fail":
-        console.log(`😱 ${type.reason}`);
+        console.log(`😱 ${state.reason}`);
         break;
       default:
         throw new Error("unknown state");
